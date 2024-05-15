a97e0d61adc3743ee7708ac177fed635515af0cf
João Eduardo Montandon <edu.montandon@gmail.com>
2024-05-15 01:54:58 -0300

    Updated remained tests as pending

--- .github/actions/css/cypress/integration/t4.test.js
+++ .github/actions/css/cypress/integration/t4.test.js
@@ -1,37 +1,37 @@
-describe('T4: Posicionamento', () => {
-    before(() => {
-      cy.visit('/index.html')
-    })
+// describe('T4: Posicionamento', () => {
+//     before(() => {
+//       cy.visit('/index.html')
+//     })
   
-    it('deve ter o .page-wrapper como referência de posicionamento', () => {
-        cy.get('.page-wrapper').should('have.css', 'position', 'relative')
-    })    
+//     it('deve ter o .page-wrapper como referência de posicionamento', () => {
+//         cy.get('.page-wrapper').should('have.css', 'position', 'relative')
+//     })    
 
-    it('deve modificar o tamanho do painel .explanation', () => {  
-        cy.get('.explanation').then(($el) => {
-          // Check width
-          const windowWidth = Cypress.$(cy.state('window')).width();
-          const elementWidth = $el.width();
-          expect(elementWidth).to.be.closeTo(windowWidth * 0.6, 1);
+//     it('deve modificar o tamanho do painel .explanation', () => {  
+//         cy.get('.explanation').then(($el) => {
+//           // Check width
+//           const windowWidth = Cypress.$(cy.state('window')).width();
+//           const elementWidth = $el.width();
+//           expect(elementWidth).to.be.closeTo(windowWidth * 0.6, 1);
     
-          // Check right margin
-          const marginRight = parseInt($el.css('marginRight'), 10);
-          expect(marginRight).to.be.closeTo(windowWidth * 0.35, 1);
+//           // Check right margin
+//           const marginRight = parseInt($el.css('marginRight'), 10);
+//           expect(marginRight).to.be.closeTo(windowWidth * 0.35, 1);
     
-          // Check right border
-          const borderRight = $el.css('borderRight');
-          expect(borderRight).to.equal('1px solid #1f295c');
+//           // Check right border
+//           const borderRight = $el.css('borderRight');
+//           expect(borderRight).to.equal('1px solid #1f295c');
     
-          // Check right padding
-          const paddingRight = parseInt($el.css('paddingRight'), 10);
-          expect(paddingRight).to.be.closeTo(elementWidth * 0.02, 1);
-        });
-    });
+//           // Check right padding
+//           const paddingRight = parseInt($el.css('paddingRight'), 10);
+//           expect(paddingRight).to.be.closeTo(elementWidth * 0.02, 1);
+//         });
+//     });
 
-    it('deve ter o menu de design na posição certa', () => {
-        cy.get('#design-archives').should('have.css', 'top', '560px');
-        cy.get('#design-archives').should('have.css', 'left', '62%');
-        cy.get('#design-archives').should('have.css', 'padding-left', '2%');
-        cy.get('#design-archives').should('have.css', 'padding-right', '2%');
-    });
-});
+//     it('deve ter o menu de design na posição certa', () => {
+//         cy.get('#design-archives').should('have.css', 'top', '560px');
+//         cy.get('#design-archives').should('have.css', 'left', '62%');
+//         cy.get('#design-archives').should('have.css', 'padding-left', '2%');
+//         cy.get('#design-archives').should('have.css', 'padding-right', '2%');
+//     });
+// });
--- .github/actions/css/cypress/integration/t6.test.js
+++ .github/actions/css/cypress/integration/t6.test.js
@@ -1,31 +1,31 @@
-describe('T6: Flexbox', () => {
-  before(() => {
-    cy.visit('/index.html')
-  })
+// describe('T6: Flexbox', () => {
+//   before(() => {
+//     cy.visit('/index.html')
+//   })
 
   
-  it('deve aplicar flexbox nos links do footer', () => {
-    cy.get('.main footer a').should('have.css', 'display', 'flex')
-      .and('have.css', 'justify-content', 'center')
-  });
+//   it('deve aplicar flexbox nos links do footer', () => {
+//     cy.get('.main footer a').should('have.css', 'display', 'flex')
+//       .and('have.css', 'justify-content', 'center')
+//   });
 
-  it('deve aplicar flexbox na seção de benefícios', () => {
-    cy.get('.benefits').should('have.css', 'display', 'flex')
-      .and('have.css', 'flex-direction', 'row-reverse');
-    cy.get('.benefits h3').should('have.css', 'align-self', 'center')
-      .and('have.css', 'flex', '0 0 30%');
-    cy.get('.benefits .text').should('have.css', 'flex', '0 0 70%');
-  });
+//   it('deve aplicar flexbox na seção de benefícios', () => {
+//     cy.get('.benefits').should('have.css', 'display', 'flex')
+//       .and('have.css', 'flex-direction', 'row-reverse');
+//     cy.get('.benefits h3').should('have.css', 'align-self', 'center')
+//       .and('have.css', 'flex', '0 0 30%');
+//     cy.get('.benefits .text').should('have.css', 'flex', '0 0 70%');
+//   });
 
-  it('deve aplicar flexbox na seção de participação', () => {
-    cy.get('.participation').should('have.css', 'display', 'flex');
-    cy.get('.participation > *').should('have.css', 'flex', '0 0 50%');
-    cy.get('.participation > h3').should('have.css', 'align-self', 'center');
-  });
+//   it('deve aplicar flexbox na seção de participação', () => {
+//     cy.get('.participation').should('have.css', 'display', 'flex');
+//     cy.get('.participation > *').should('have.css', 'flex', '0 0 50%');
+//     cy.get('.participation > h3').should('have.css', 'align-self', 'center');
+//   });
 
-  it('deve incluir linha entre as seções de benefícios e participação', () => {
-    cy.get('.benefits').should('have.css', 'border-bottom');
-    cy.get('.participation').should('have.css', 'border-top');
-  });
+//   it('deve incluir linha entre as seções de benefícios e participação', () => {
+//     cy.get('.benefits').should('have.css', 'border-bottom');
+//     cy.get('.participation').should('have.css', 'border-top');
+//   });
 
-});
+// });
