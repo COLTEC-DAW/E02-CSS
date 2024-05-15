bf6968c16dc4b0cfe09b48cebae467547c265dee
João Eduardo Montandon <edu.montandon@gmail.com>
2024-05-15 13:21:51 -0300

    Fixed cypress environment variable

--- .github/actions/css/cypress.config.js
+++ .github/actions/css/cypress.config.js
@@ -1,16 +1,18 @@
+const cypressPath = process.env.CYPRESS_PATH ? process.env.CYPRESS_PATH : '.';
+
 module.exports = {
   e2e: {
     baseUrl: "http://localhost:8080",
-    specPattern: `${process.env.cypressPath}/cypress/integration/**/*.test.js`,
+    specPattern: `${cypressPath}/cypress/integration/**/*.test.js`,
     video: true,
-    screenshotsFolder: `${process.env.cypressPath}/cypress/screenshots`,
-    videosFolder: `${process.env.cypressPath}/cypress/videos`,
+    screenshotsFolder: `${cypressPath}/cypress/screenshots`,
+    videosFolder: `${cypressPath}/cypress/videos`,
     viewportWidth: 1280,
     viewportHeight: 720,
     supportFile: false,
     reporter: 'junit',
     reporterOptions: {
-      mochaFile: `${process.env.cypressPath}/cypress/reports/report-[hash].xml`
+      mochaFile: `${cypressPath}/cypress/reports/report-[hash].xml`
     },
   }
 }
