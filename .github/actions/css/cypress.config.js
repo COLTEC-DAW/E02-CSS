21359acd22d89a7b7b8a46848b0fa2ddad5eae95
João Eduardo Montandon <edu.montandon@gmail.com>
2024-05-15 00:47:11 -0300

    Updated configuration

--- .github/actions/css/cypress.config.js
+++ .github/actions/css/cypress.config.js
@@ -1,16 +1,18 @@
+const cypressPath = process.env.cypressPath || ".";
+
 module.exports = {
   e2e: {
     baseUrl: "http://localhost:8080",
-    specPattern: `${process.env.CYPRESS_PATH}/cypress/integration/**/*.test.js`,
+    specPattern: `${cypressPath}/cypress/integration/**/*.test.js`,
     video: true,
-    screenshotsFolder: `${process.env.CYPRESS_PATH}/cypress/screenshots`,
-    videosFolder: `${process.env.CYPRESS_PATH}/cypress/videos`,
+    screenshotsFolder: `${cypressPath}/cypress/screenshots`,
+    videosFolder: `${cypressPath}/cypress/videos`,
     viewportWidth: 1280,
     viewportHeight: 720,
     supportFile: false,
     reporter: 'junit',
     reporterOptions: {
-      mochaFile: `${process.env.CYPRESS_PATH}/cypress/reports/report-[hash].xml`
+      mochaFile: `${cypressPath}/cypress/reports/report-[hash].xml`
     },
   }
 }
