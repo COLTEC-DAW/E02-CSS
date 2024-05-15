46577dc309887cee5ee472aa315fd801b6f5639e
João Eduardo Montandon <edu.montandon@gmail.com>
2024-05-15 00:49:32 -0300

    Fixed t1

--- .github/actions/css/cypress/integration/t1.test.js
+++ .github/actions/css/cypress/integration/t1.test.js
@@ -2,10 +2,11 @@ describe('T1: Paleta de Cores', () => {
   before(() => {
     cy.visit('/index.html')
   })
-  it('Deve ter uma cor customizada para o texto da página', () => {
-    cy.get('body, html, .page-wrapper').should('have.css', 'background-color').and('not.equal', 'rgb(255, 255, 255)')
-  })
   it('Deve ter uma cor customizada para o fundo da página', () => {
-    cy.get('body, html, .page-wrapper').should('have.css', 'color').and('not.equal', 'rgb(0, 0, 0)')
+    console.log()
+    cy.get('body').should('have.css', 'background-color').and('not.equal', 'rgba(0, 0, 0, 0)')
+  })
+  it('Deve ter uma cor customizada para o texto da página', () => {
+    cy.get('body').should('have.css', 'color').and('not.equal', 'rgb(46, 50, 71)')
   })
 });
