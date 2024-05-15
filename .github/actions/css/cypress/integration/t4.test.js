8e51ede0e8a7958e2438824056157fd8e9c1b401
João Eduardo Montandon <edu.montandon@gmail.com>
2024-05-15 09:50:16 -0300

    FIxed t4

--- .github/actions/css/cypress/integration/t4.test.js
+++ .github/actions/css/cypress/integration/t4.test.js
@@ -1,37 +1,52 @@
-// describe('T4: Posicionamento', () => {
-//     before(() => {
-//       cy.visit('/index.html')
-//     })
+describe('T4: Posicionamento', () => {
+    beforeEach(() => {
+      cy.visit('/index.html')
+    })
   
-//     it('deve ter o .page-wrapper como referência de posicionamento', () => {
-//         cy.get('.page-wrapper').should('have.css', 'position', 'relative')
-//     })    
+    it('deve ter o .page-wrapper como referência de posicionamento', () => {
+        cy.get('.page-wrapper').should('have.css', 'position', 'relative')
+    })    
 
-//     it('deve modificar o tamanho do painel .explanation', () => {  
-//         cy.get('.explanation').then(($el) => {
-//           // Check width
-//           const windowWidth = Cypress.$(cy.state('window')).width();
-//           const elementWidth = $el.width();
-//           expect(elementWidth).to.be.closeTo(windowWidth * 0.6, 1);
+    it('deve modificar o tamanho do painel .explanation', () => {  
+        cy.get('.explanation').then(($el) => {
+          // Check width
+          const windowWidth = Cypress.$(cy.state('window')).width();
+          const elementWidth = $el.width();
+          expect(elementWidth).to.be.at.most(windowWidth * 0.6, 1);
     
-//           // Check right margin
-//           const marginRight = parseInt($el.css('marginRight'), 10);
-//           expect(marginRight).to.be.closeTo(windowWidth * 0.35, 1);
+          // Check right margin
+          const marginRight = parseInt($el.css('marginRight'), 10);
+          expect(marginRight).to.be.at.most(windowWidth * 0.35, 1);
     
-//           // Check right border
-//           const borderRight = $el.css('borderRight');
-//           expect(borderRight).to.equal('1px solid #1f295c');
+          // Check right border
+          const borderRight = $el.css('borderRight');
+          expect(borderRight).to.not.contain('0px');
+          expect(borderRight).to.contain('solid');
     
-//           // Check right padding
-//           const paddingRight = parseInt($el.css('paddingRight'), 10);
-//           expect(paddingRight).to.be.closeTo(elementWidth * 0.02, 1);
-//         });
-//     });
+          // Check right padding
+          const paddingRight = parseInt($el.css('paddingRight'), 10);
+          expect(paddingRight).to.be.greaterThan(0);
+        });
+    });
 
-//     it('deve ter o menu de design na posição certa', () => {
-//         cy.get('#design-archives').should('have.css', 'top', '560px');
-//         cy.get('#design-archives').should('have.css', 'left', '62%');
-//         cy.get('#design-archives').should('have.css', 'padding-left', '2%');
-//         cy.get('#design-archives').should('have.css', 'padding-right', '2%');
-//     });
-// });
+    it('deve ter o menu de design na posição certa', () => {
+      cy.get('#design-archives').should(($el) => {
+        const windowWidth = Cypress.$(cy.state('window')).width();
+
+        const style = window.getComputedStyle($el[0]);
+        const top = parseInt(style.top.replace('px', ''), 10);
+        const left = parseInt(style.left.replace('%', ''), 10);
+        const paddingRight = parseInt(style.paddingRight, 10);
+        const paddingLeft = parseInt(style.paddingLeft, 10);
+        const position = style.position;
+
+
+        expect(position).to.be.equals('absolute');
+        expect(top).to.be.at.least(400);
+        expect(left).to.be.at.least(50);
+        expect(paddingRight).to.be.greaterThan(0);
+        expect(paddingLeft).to.be.greaterThan(0);
+
+      });
+    });
+});
--- README.md
+++ README.md
@@ -96,7 +96,7 @@ O foco dessa atividade será reposicionar os elementos da nossa página!
 
 1. Defina o elemento `.page-wrapper` como referência para posicionamento.
 2. Faça com que o painel `.explanation` ocupe 60% da largura disponível, mas crie um espaçamento a direita de 35% utilizando `margin`.  
-    - Adicione uma borda à direita do painel de 1px, com a cor `#1f295c`  
+    - Adicione uma borda à direita do painel de 1px  
     - Adicione um espaçamento interno de 2% no lado direito do painel.
 3. Agora mova o menu de designs para a direita da página, ocupando o espaço em branco deixado pelo elemento `.explanation`.  
     - Faça-o iniciar na parte superior da página, após a parte de introdução (por exemplo, aplicando `top: 560px`).  
